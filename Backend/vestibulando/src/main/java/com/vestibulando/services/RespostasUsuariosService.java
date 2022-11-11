0ac3751eb55b9f1c68402094fb02dece08c53c99
f0fp1422 <viniciusnandes@hotmail.com>
2022-11-11 15:15:27 -0300

    Respostas usuario CRUD

---
+++ Backend/vestibulando/src/main/java/com/vestibulando/services/RespostasUsuariosService.java
@@ -0,0 +1,54 @@
+package com.vestibulando.services;
+
+import com.vestibulando.entities.RespostasUsuarios;
+import com.vestibulando.entities.Simulado;
+import com.vestibulando.repositories.IRespostasUsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class RespostasUsuariosService {
+    @Autowired
+    IRespostasUsuariosRepository respostasUsuariosRepository;
+
+    public Page<RespostasUsuarios> listar(Pageable page){
+        Page<RespostasUsuarios> pagina = respostasUsuariosRepository.findAll(page);
+        return pagina;
+    }
+    public List<RespostasUsuarios> listar(long idSimulado){
+
+        Simulado simulado = new Simulado();
+        simulado.setId(idSimulado);
+       List<RespostasUsuarios> resps = respostasUsuariosRepository.findBySimulado(simulado);
+        return resps;
+    }
+
+    public RespostasUsuarios salvar(RespostasUsuarios respostaUsuario){
+        return respostasUsuariosRepository.save(respostaUsuario);
+    }
+
+
+
+    public RespostasUsuarios obter(long id){
+        RespostasUsuarios resp = respostasUsuariosRepository.findById(id).orElseThrow(()->new EntityNotFoundException("Não foi possível encontrar as respostas do usuários com esse id de respostas usuários"));
+        return resp;
+    }
+
+
+    public RespostasUsuarios editar(long id,RespostasUsuarios respostasUsuarios){
+        RespostasUsuarios respUsuario = this.obter(id);
+        respUsuario.setRespostas(respostasUsuarios.getRespostas());
+        return this.salvar(respUsuario);
+
+    }
+
+    public void deletar(long id){
+        RespostasUsuarios respUsuarios = this.obter(id);
+        respostasUsuariosRepository.delete(respUsuarios);
+    }
+}
