554e4ecdd10ff5105255954438ac07b7b6c7d46e
Thomas Mendes Carballo <f0fp1428@t-systems.com.br>
2022-11-17 14:58:11 -0300

    Teste do usuario Controller

---
+++ Backend/vestibulando/src/test/java/com/vestibulando/controllers/UsuarioControllerTests.java
@@ -0,0 +1,95 @@
+package com.vestibulando.controllers;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.vestibulando.entities.Usuario;
+
+import com.vestibulando.services.UsuarioService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UsuarioControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UsuarioService usuarioService;
+
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private String usuarioString;
+
+    @BeforeEach
+    public void beforeEach() throws JsonProcessingException {
+        Usuario usuario = new Usuario();
+        usuario.setNome("Maria");
+        usuario.setEmail("maria@email.com");
+        usuario.setSenha("123456");
+        usuario.setId(1l);
+
+        usuarioString = objectMapper.writeValueAsString(usuario);
+
+    }
+
+    @Test
+    public void RetornaCreatedQuandoUsuarioCriado() throws Exception {
+        ResultActions result = mockMvc.perform(post("/usuarios")
+                .content(usuarioString)
+                .contentType(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isCreated());
+
+    }
+
+    @Test
+    public void ListarUsuarioRetornaOk() throws Exception {
+        mockMvc.perform(get("/usuarios")
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void ObterUsuarioPorIdRetornaOk()throws Exception {
+        mockMvc.perform(get("/usuarios/{idUsuario}", 1l)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void AlterarUsuarioRetornaOk()throws Exception {
+        ResultActions result = mockMvc.perform(put("/usuarios/{idUsuario}", 1l)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(usuarioString)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+                result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void DeletarUsuarioRetornaNada() throws Exception{
+        mockMvc.perform(delete("/usuarios/{idUsuario}", 1l)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent());
+    }
+}
