cb8b61b8e590bdc9c5ca622e9d2efa4faa3b539c
Gustavo Guerreiro <f0fp1431@t-systems.com.br>
2022-11-17 08:22:29 -0300

    Teste da banca iniciado

---
+++ Backend/vestibulando/src/test/java/com/vestibulando/controllers/BancaControllerTest.java
@@ -0,0 +1,13 @@
+package com.vestibulando.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public class BancaControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+}
