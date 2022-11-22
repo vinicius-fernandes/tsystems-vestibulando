42b298cb8127527a59505db30aea28f05d3f6368
f0fp1422 <viniciusnandes@hotmail.com>
2022-11-22 15:24:35 -0300

    Corrigido a interface

--- Frontend/app-vestibulando/src/app/componentes/perguntas/listar/listar.component.ts
+++ Frontend/app-vestibulando/src/app/componentes/perguntas/listar/listar.component.ts
@@ -8,14 +8,15 @@ import { PerguntasService } from 'src/app/services/perguntas.service';
   styleUrls: ['./listar.component.css']
 })
 export class ListarComponent implements OnInit{
+
   materias:IMateria[]=[]
 
 constructor(private service:PerguntasService){
 
 }
   ngOnInit(): void {
-     this.service.consultar().subscribe(dados=>console.log(dados))
-     console.log(this.materias)
+     this.service.consultar().subscribe(dados=>this.materias  = dados)
+     console.log(JSON.stringify(this.materias.toString()))
   }
 
 
