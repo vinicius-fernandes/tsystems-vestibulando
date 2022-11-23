744ce2e131bb4c5764f32e5879513bc279984ce6
f0fp1422 <viniciusnandes@hotmail.com>
2022-11-23 09:30:14 -0300

    Simulado service criado

--- Backend/vestibulando/src/main/java/com/vestibulando/dtos/MateriaDTO.java
+++ Backend/vestibulando/src/main/java/com/vestibulando/dtos/MateriaDTO.java
@@ -1,2 +1,27 @@
-package com.vestibulando.dtos;public class MateriaDTO {
+package com.vestibulando.dtos;
+
+public class MateriaDTO {
+    private long id;
+    private String nome;
+
+    public MateriaDTO(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
