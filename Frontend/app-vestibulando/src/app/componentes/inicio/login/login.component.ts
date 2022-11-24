80668114c5170dcc0aab00adfae389dd8f7243fb
Gustavo Guerreiro <Gustavo.Guerreiro@proway.treina>
2022-11-24 11:12:02 -0300

    Lista de seleção da página de gerar simulado alinhado

--- Frontend/app-vestibulando/src/app/componentes/inicio/login/login.component.ts
+++ Frontend/app-vestibulando/src/app/componentes/inicio/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private _router: Router) { }
 
+  redirecionarCadastro() {
+    this._router.navigateByUrl('/cadastro')
+  }
+
+  redirecionarGerarSimulado() {
+    this._router.navigateByUrl('/app/gerarSimulado')
+  }
 }
