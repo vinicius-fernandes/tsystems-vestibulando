16d5919eb37e65f911e447e2c7f9e50235e9bb17
Teshi Kauê Tsurumi Báfica <teshi.kaue@gmail.com>
2022-11-25 16:04:08 -0300

    Adicionada mensagem Toastr e redirecionamento para a home caso não consiga consultar os usuários

--- Frontend/app-vestibulando/src/app/componentes/usuarios/usuarios.component.ts
+++ Frontend/app-vestibulando/src/app/componentes/usuarios/usuarios.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import IUsuario from 'src/app/interfaces/IUsuario';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-usuarios',
@@ -12,18 +13,25 @@ export class UsuariosComponent {
 
   public users: IUsuario[] = []
 
-  constructor(private service: UsuarioService, private toastr: ToastrService) {
+  constructor(private service: UsuarioService, private toastr: ToastrService, private router: Router) {
     this.consultar()
   }
   consultar() {
-    this.service.consultar().subscribe(data => this.users = data)
+    this.service.consultar().subscribe({
+      next: data => this.users = data,
+      error: erro => {
+        console.log(erro)
+        this.toastr.error("Não foi possível consultar os usuários.", "Erro")
+        this.router.navigate(['app', 'home'])
+      }
+    })
   }
   excluir(idUsuario: any) {
     this.service.excluir(idUsuario).subscribe({next: () => {
       this.users = this.users.filter(u => u.id != idUsuario)
-      this.toastr.success('Usuario excluída com sucesso!', 'Sucesso')
+      this.toastr.success('Usuario excluído com sucesso!', 'Sucesso')
     }, error: (erro) => {
-    this.toastr.error('Este usuário não pode ser excluída, há objetos ligados a ela.', 'Erro')
+    this.toastr.error('Este usuário não pode ser excluído, há objetos ligados a ele.', 'Erro')
     }})
   }
 
