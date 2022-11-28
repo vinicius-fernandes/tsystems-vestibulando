56dfe07ab8efc0ec18473a5b586c897e5c5e6b60
Gustavo Guerreiro <Gustavo.Guerreiro@proway.treina>
2022-11-28 16:49:43 -0300

    Feito menu principal, alterado icone da lista de simulados

--- Frontend/app-vestibulando/src/app/componentes/layout/sidebar/sidebar.component.ts
+++ Frontend/app-vestibulando/src/app/componentes/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
-
 }
