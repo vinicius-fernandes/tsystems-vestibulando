b0cd4bf786f69e97512497829abc3ae09664dcb3
f0fp1422 <viniciusnandes@hotmail.com>
2022-11-30 13:39:23 -0300

    Autorização pelo swagger ok

--- Backend/vestibulando/src/main/java/com/vestibulando/config/SwaggerConfig.java
+++ Backend/vestibulando/src/main/java/com/vestibulando/config/SwaggerConfig.java
@@ -12,18 +12,50 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @EnableSwagger2
 @EnableSpringDataWebSupport
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "API Vestibulando",
+                "API da vestibulando",
+                "1.0",
+                "Terms of service",
+                new Contact("Vestibulando", "www.vestibulando.com", "vestibulando@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
+    }
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
@@ -31,8 +63,9 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
