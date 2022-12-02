25be4101074d19f5f7d6eb1324989d40423db0a5
RafaGrassi <rafa_grassi@live.com>
2022-12-02 09:54:50 -0300

    Commit de teste

--- Backend/vestibulando/src/main/java/com/vestibulando/repositories/IPerguntaRepository.java
+++ Backend/vestibulando/src/main/java/com/vestibulando/repositories/IPerguntaRepository.java
@@ -48,7 +48,4 @@ public interface IPerguntaRepository extends JpaRepository<Pergunta, Long> {
     Page<Pergunta> findByCorpoIgnoreCaseContaining(String corpo,Pageable page);
     Page<Pergunta> findByCorpoIgnoreCaseContainingAndBancaAndMateria(String corpo, Banca banca, Materia materia, Pageable page);
 
-//    @Query("select p from Pergunta p where p.materia.id IN ?1 and p.banca.id IN ?2")
-//    Page<Pergunta> getPerguntasFiltro(List<Long> idMaterias, List<Long> idBancas, Pageable pageable);
-
 }
