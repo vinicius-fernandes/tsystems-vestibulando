b56e67f29bc35a0782d10494a94a2ec4d5a0dfb0
f0fp1422 <viniciusnandes@hotmail.com>
2022-12-06 14:26:50 -0300

    Correção environment

---
+++ Frontend/app-vestibulando/src/environment.prod.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://tsystems-vesti-prod-vestibulando-ffanlh.mo2.mogenius.io/',
+  OAUTH_CLIENT: 'tsystems',
+  OAUTH_SECRET: 'tsystems123'
+}
--- Frontend/app-vestibulando/src/environment.ts
+++ Frontend/app-vestibulando/src/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: process.env["API_URL"]? process.env["API_URL"]: "http://localhost:8080",
+  apiUrl: "http://localhost:8080",
   OAUTH_CLIENT: 'tsystems',
   OAUTH_SECRET: 'tsystems123'
 }
