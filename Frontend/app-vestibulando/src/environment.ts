cb36fef479b2fc683bbc8043c10bc7800c1a2362
f0fp1422 <viniciusnandes@hotmail.com>
2022-12-06 14:08:14 -0300

    Change docker file front end

--- Frontend/app-vestibulando/src/environment.ts
+++ Frontend/app-vestibulando/src/environment.ts
@@ -1,6 +1,7 @@
+
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:8080",
+  apiUrl: process.env["API_URL"]? process.env["API_URL"]: "http://localhost:8080",
   OAUTH_CLIENT: 'tsystems',
   OAUTH_SECRET: 'tsystems123'
 }
