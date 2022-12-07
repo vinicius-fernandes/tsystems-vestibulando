927aae4a5a3e1aab615ee1007cf4d23118327cf0
f0fp1422 <viniciusnandes@hotmail.com>
2022-12-07 14:54:01 -0300

    Paginação e pesquisa de simulados

--- Frontend/app-vestibulando/src/app/componentes/lista-simulados/lista-simulados.component.ts
+++ Frontend/app-vestibulando/src/app/componentes/lista-simulados/lista-simulados.component.ts
@@ -8,6 +8,11 @@ import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent, ConfirmDialogModel } from '../confirm-dialog/confirm-dialog.component';
 import { RespostasUsuariosService } from 'src/app/services/respostas-usuarios.service';
 import INotasSimuladosUsuarios from 'src/app/interfaces/INotasSimuladosUsuario';
+import { PageEvent } from '@angular/material/paginator';
+import IGeneric from 'src/app/interfaces/IGeneric';
+import { BancasService } from 'src/app/services/bancas.service';
+import { MateriasService } from 'src/app/services/materias.service';
+import IPesquisaSimuladoDTO from 'src/app/interfaces/IPesquisaSimuladoDTO';
 
 @Component({
   selector: 'app-lista-simulados',
@@ -19,7 +24,15 @@ export class ListaSimuladosComponent implements OnInit {
   simulados: ISimulado[] = []
   resultadosUsuario: INotasSimuladosUsuarios[] = []
 
+  dataTest:IGeneric[]=[{id:1,name:'batata'},{id:2,name:'carne'}]
+
+  materiasFiltro:IGeneric[]=[]
+  bancasFiltro:IGeneric[]=[]
+
+  bancasFiltroSelecionadas:number[]=[]
+  materiasFiltroSelecionadas:number[]=[]
   isAdmin: boolean = false;
+  totalElements: number = 0;
 
   loading:boolean=true
   constructor(private _router: Router,
@@ -27,6 +40,8 @@ export class ListaSimuladosComponent implements OnInit {
     private toastr: ToastrService,
     private router: Router,
     private respostasUsuarioService: RespostasUsuariosService,
+    private bancasService:BancasService,
+    private materiasService:MateriasService,
     private jwtService: JwtTokenService,
     private dialog: MatDialog) {
   }
@@ -38,7 +53,77 @@ export class ListaSimuladosComponent implements OnInit {
   ngOnInit() {
     this.isAdmin = this.jwtService.checkAuthoritie('ROLE_ADMIN')
     this.obterRespostasUsuario()
-    this.criarLista()
+    this.consultar({ page: '0', size: '3' })
+    this.carregarBancas();
+    this.carregarMaterias();
+  }
+
+  nextPage(event: PageEvent) {
+    const request = {
+      page: event.pageIndex.toString(),
+      size: event.pageSize.toString(),
+    };
+    this.carregarSimulados(request);
+  }
+
+  carregarBancas(){
+    this.bancasService.consultar().subscribe({
+      next:(bancas)=>{
+        this.bancasFiltro=bancas.map((b)=><IGeneric>{id:b.id,name:b.sigla})
+      },
+      error:(erro)=>{
+        console.log(erro)
+      }
+    })
+  }
+
+  carregarMaterias(){
+    this.materiasService.consultar().subscribe({
+      next:(materias)=>{
+        this.materiasFiltro=materias.map((m)=><IGeneric>{id:m.id,name:m.nome})
+      },
+      error:(erro)=>{
+        console.log(erro)
+      }
+    })
+  }
+
+  consultar(params:any){
+    this.service.listarPaginado(params).subscribe(
+      {
+        next:(value)=>{
+          this.simulados=<ISimulado[]>value.content
+          this.totalElements=value.totalElements
+        },
+        error:(erro)=>{
+          console.log(erro)
+          if (erro.status != 401) {
+
+            this.toastr.error("Não foi possível consultar os simulados.", "Erro")
+            this.router.navigate(['app', 'home'])
+          }
+        }
+      }
+    ).add(()=>this.loading=false)
+  }
+
+  pesquisar(params:any){
+    let pesquisaDto : IPesquisaSimuladoDTO = {idBancas:this.bancasFiltroSelecionadas,idMaterias:this.materiasFiltroSelecionadas}
+
+    this.service.pesquisar(params,pesquisaDto).subscribe({
+      next:(value)=>{
+        this.simulados=<ISimulado[]>value.content
+        this.totalElements=value.totalElements
+      },
+      error:(erro)=>{
+        console.log(erro)
+        if (erro.status != 401) {
+
+          this.toastr.error("Não foi possível consultar os simulados.", "Erro")
+          this.router.navigate(['app', 'home'])
+        }
+      }
+    }).add(()=>this.loading=false)
   }
 
   encontrarSimuladoRespondido(idSimulado: number) {
@@ -64,23 +149,7 @@ export class ListaSimuladosComponent implements OnInit {
     }
   }
 
-  criarLista() {
-    this.service.listar()
-      .subscribe({
-        next: dados => this.simulados = dados.sort(
-          (a, b) => {
-            return b.createdAt - a.createdAt
-          }),
-        error: erro => {
-          console.log(erro)
-          if (erro.status != 401) {
 
-            this.toastr.error("Não foi possível consultar os simulados.", "Erro")
-            this.router.navigate(['app', 'home'])
-          }
-        }
-      }).add(()=>this.loading=false)
-  }
 
   confirmarExclusaoSimulado(id: number) {
     const dialogData = new ConfirmDialogModel("Confirmar exclusão", "Tem certeza de que deseja excluir este simulado?")
@@ -94,6 +163,30 @@ export class ListaSimuladosComponent implements OnInit {
         this.excluirSimulado(id)
       }
     })
+  }
+
+  filtroBancasMudou(selecionados:number[]){
+    console.log(selecionados)
+    this.bancasFiltroSelecionadas=selecionados;
+    this.carregarSimulados({ page: '0', size: '3' })
+
+  }
+
+  filtroMateriaMudou(selecionados:number[]){
+    console.log(selecionados)
+    this.materiasFiltroSelecionadas=selecionados
+    this.carregarSimulados({ page: '0', size: '3' })
+  }
+
+  carregarSimulados(params:any){
+    this.loading=true
+    this.simulados=[]
+    if(this.bancasFiltroSelecionadas.length==0 && this.materiasFiltroSelecionadas.length==0){
+      this.consultar(params);
+      return
+    }
+    this.pesquisar(params)
+
   }
 
 
