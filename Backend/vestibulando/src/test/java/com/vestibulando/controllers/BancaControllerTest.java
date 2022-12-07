ab8171a5f68d1a02e3139c55898ae2538f626466
f0fp1422 <viniciusnandes@hotmail.com>
2022-12-07 08:32:42 -0300

    Correção nos testes

--- Backend/vestibulando/src/test/java/com/vestibulando/controllers/BancaControllerTest.java
+++ Backend/vestibulando/src/test/java/com/vestibulando/controllers/BancaControllerTest.java
@@ -2,10 +2,12 @@ package com.vestibulando.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vestibulando.entities.Banca;
+import com.vestibulando.services.BancaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +24,9 @@ public class BancaControllerTest {
     MockMvc mockMvc;
     @Autowired
     ObjectMapper objectMapper;
+
+    @MockBean
+    BancaService bancaService;
 
     @Test
     public void ListarBancasRetornaOk() throws Exception {
